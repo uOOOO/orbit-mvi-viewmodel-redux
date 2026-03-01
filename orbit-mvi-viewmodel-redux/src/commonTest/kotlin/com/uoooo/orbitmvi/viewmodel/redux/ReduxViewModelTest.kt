1ebca627ebe29d36409999447fd96bde23522c8c
Seunghun Choe <13363349+uOOOO@users.noreply.github.com>
2026-03-02 00:48:20 +0900

    test: update tests

--- orbit-mvi-viewmodel-redux/src/commonTest/kotlin/com/uoooo/orbitmvi/viewmodel/redux/ReduxViewModelTest.kt
+++ orbit-mvi-viewmodel-redux/src/commonTest/kotlin/com/uoooo/orbitmvi/viewmodel/redux/ReduxViewModelTest.kt
@@ -1,5 +1,7 @@
 package com.uoooo.orbitmvi.viewmodel.redux
 
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
 import org.orbitmvi.orbit.test.test
 import kotlin.test.Test
@@ -14,14 +16,15 @@ class ReduxViewModelTest {
         }
     }
 
-    private val testMiddleware = middleware<TestState, TestAction, TestChange, TestSideEffect> { action, _ ->
-        when (action) {
-            TestAction.ClickPlus -> change(TestChange.Increment)
-            TestAction.ClickMinus -> change(TestChange.Decrement)
-            TestAction.TriggerToast -> sideEffect(TestSideEffect.ShowToast("Hello"))
-            TestAction.ChainAction -> dispatch(TestAction.ClickPlus)
+    private val testMiddleware =
+        middleware<TestState, TestAction, TestChange, TestSideEffect> { action, _ ->
+            when (action) {
+                TestAction.ClickPlus -> change(TestChange.Increment)
+                TestAction.ClickMinus -> change(TestChange.Decrement)
+                TestAction.TriggerToast -> sideEffect(TestSideEffect.ShowToast("Hello"))
+                TestAction.ChainAction -> dispatch(TestAction.ClickPlus)
+            }
         }
-    }
 
     @Test
     fun `dispatch ClickPlus action updates state correctly via Change`() = runTest {
@@ -63,6 +66,69 @@ class ReduxViewModelTest {
             containerHost.dispatch(TestAction.ChainAction)
 
             expectState { copy(count = 1) }
+
+            job.cancel()
+        }
+    }
+
+    @Test
+    fun `FlowCollector extension functions work correctly`() = runTest {
+        val flowMiddleware =
+            middleware<TestState, TestAction, TestChange, TestSideEffect> { action, _ ->
+                when (action) {
+                    TestAction.ChainAction -> flow {
+                        change(TestChange.Increment)
+                        sideEffect(TestSideEffect.ShowToast("Flow"))
+                        dispatch(TestAction.ClickPlus)
+                    }
+
+                    TestAction.ClickPlus -> change(TestChange.Increment)
+                    else -> emptyFlow()
+                }
+            }
+
+        val viewModel = TestViewModel(listOf(flowMiddleware), testReducer)
+
+        viewModel.test(this) {
+            val job = runOnCreate()
+
+            containerHost.dispatch(TestAction.ChainAction)
+
+            expectState { copy(count = 1) }
+            expectSideEffect(TestSideEffect.ShowToast("Flow"))
+            expectState { copy(count = 2) }
+
+            job.cancel()
+        }
+    }
+
+    @Test
+    fun `multiple middlewares process actions independently`() = runTest {
+        val plusMiddleware =
+            middleware<TestState, TestAction, TestChange, TestSideEffect> { action, _ ->
+                when (action) {
+                    TestAction.ClickPlus -> change(TestChange.Increment)
+                    else -> emptyFlow()
+                }
+            }
+        val minusMiddleware =
+            middleware<TestState, TestAction, TestChange, TestSideEffect> { action, _ ->
+                when (action) {
+                    TestAction.ClickMinus -> change(TestChange.Decrement)
+                    else -> emptyFlow()
+                }
+            }
+
+        val viewModel = TestViewModel(listOf(plusMiddleware, minusMiddleware), testReducer)
+
+        viewModel.test(this) {
+            val job = runOnCreate()
+
+            containerHost.dispatch(TestAction.ClickPlus)
+            expectState { copy(count = 1) }
+
+            containerHost.dispatch(TestAction.ClickMinus)
+            expectState { copy(count = 0) }
 
             job.cancel()
         }
